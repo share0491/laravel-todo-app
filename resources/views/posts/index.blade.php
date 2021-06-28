694a5027ce431b40fcb553231fbcab7bc076e23b
share0491 <66313007+share0491@users.noreply.github.com>
2021-06-28 15:09:54 +0800

    44 Laughably Simple Pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,9 +2,10 @@
 @include('posts/_header')
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    
+
     @if($posts->count())
        <x-posts-grid :posts="$posts"/>
+       {{ $posts->links()}}
     @else
         <P class="text-center">No Posts</P>
     @endif
